f2308db79c62a934b4452e40b7d5a5de6dbde8f9
zxlfly <zxlfly@foxmail.com>
2023-06-23 10:23:58 +0800

    ico

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -29,7 +29,7 @@ export default defineConfig({
 			{
 				rel: "shortcut icon",
 				type: "image/x-icon",
-				href: "/favicon.ico",
+				href: "/theRoadIHaveTaken/img/favicon.ico",
 			},
 		],
 	],
